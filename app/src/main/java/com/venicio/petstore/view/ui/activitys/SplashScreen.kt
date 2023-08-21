d21512a3504b98b0ba399aa97e2bf54a8c64c20f
DEVenicio <venicio.almeida@hotmail.com>
2023-08-21 12:36:30 -0300

    Feat: Create project
    - add viewBinding
    - create Splash Screen

---
+++ app/src/main/java/com/venicio/petstore/view/ui/activitys/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.venicio.petstore.view.ui.activitys
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.venicio.petstore.R
+import com.venicio.petstore.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
+        }, 3000)
+    }
+
+}
